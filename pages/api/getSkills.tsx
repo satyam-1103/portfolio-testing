f984a60f023f4f194b1871ab6400fdaad7f2284e
Satyam Srivastava <satyamsrivastav435@gmail.com>
2022-11-17 20:53:23 +0530

    changes to getSkills

--- pages/api/getSkills.tsx
+++ pages/api/getSkills.tsx
@@ -7,13 +7,13 @@ const query = groq`
     *[_type == "skill"]`;
 
 type Data = {
-  skills: Skill[];
+  skills: SkillType[];
 };
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  const skills: Skill[] = await sanityClient.fetch(query);
+  const skills: SkillType[] = await sanityClient.fetch(query);
   res.status(200).json({ skills });
 }
